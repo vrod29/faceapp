89e331b82e1cdf768c42d6c89de4ee656bf6bcac
Unknown <vmrdz220@gmail.com>
2018-03-19 15:20:51 -0500

    added master and index yield and some  design

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,28 @@
+@extends('welcome')
 
+@section('title')
+@endsection
+
+@section('content')
+    <section id="top" class="jumbotron">
+      <div class="containier">
+        <h1 class="display-1">FaceApp</h1>
+        <p class="display-4">Capture every moment</p>
+        <p class="lead">Create stunning photos and videos by using the built in features for enhancement</p>
+        <p class="lead">Share your best shots with your friends and the rest of the world instantly.</p>
+        <p><strong>Download now on</strong></p>
+        <a href="#" class="btn btn-lg btn-primary">App Store</a>
+        <a href="#" class="btn btn-lg btn-primary">Google Play</a>
+      </div>
+    </section>
+@endsection
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
