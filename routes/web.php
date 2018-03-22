1226b3e235108448c735acaa111d56ada192d459
Unknown <vmrdz220@gmail.com>
2018-03-22 13:53:22 -0500

    added logout function to route and controller

--- routes/web.php
+++ routes/web.php
@@ -44,3 +44,5 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::post('/getFaceAppResults', 'FaceController@getFaceAppResults');
+
+Route::get('/logout', 'HomeController@logout');
