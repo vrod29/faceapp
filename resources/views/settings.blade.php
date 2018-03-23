fb93e92380b689c6591b813eaa16def56ab67592
Unknown <vmrdz220@gmail.com>
2018-03-23 11:11:54 -0500

    styled settings page

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -1,19 +1,27 @@
-@extends('master') @section('content')
-<div class="row">
-    <div class="col">
-        <form action="updateSettings" method="post">
-            @csrf
-            <div class="form-group">
-                <label for="exampleInputEmail1">Change Name</label>
-                <input name="name"  class="form-control"  aria-describedby="nameHelp" placeholder="Enter name" value="{{ $userSettings->name }}">
-                <label for="exampleInputEmail1">Change Email</label>
-                <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" value="{{ $userSettings->email }}">
+@extends('master')
 
+@section('content')
+  <div class="container">
+    <div class="row justify-content-center mt-5">
+      <div class="col-8">
+        <div class="card">
+          <div class="card-header">
+            <h6>Change User Settings</h6>
+            <div class="card-body">
+              <form action="updateSettings" method="post">
+                  @csrf
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Change Name</label>
+                      <input name="name"  class="form-control mb-3"  aria-describedby="nameHelp" placeholder="Enter name" value="{{ $userSettings->name }}">
+                    <label for="exampleInputEmail1">Change Email</label>
+                      <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" value="{{ $userSettings->email }}">
+                  </div>
+                  <button type="submit" class="btn btn-primary">Submit</button>
+              </form>
             </div>
-
-
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
+          </div>
+        </div>
+      </div>
     </div>
-</div>
+  </div>
 @endsection
